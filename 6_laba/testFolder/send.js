34c594d889c6a6a7f7915d72345f675e57abce9f
fmkpl <efimkopyltoppg@yandex.by>
2021-10-26 16:23:39 +0300

    6 laba done

---
+++ 6_laba/testFolder/send.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+const transport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "efimberg22@gmail.com",
+    pass: "LLW-XNG-Nny-3Gw",
+  },
+});
+
+function send(mail) {
+  const option = {
+    from: "efimberg22@gmail.com",
+    to: "efimkopyltoppg@yandex.by",
+    subject: "Send function",
+    text: mail,
+  };
+
+  transport.sendMail(option, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+}
+
+module.exports = send;
